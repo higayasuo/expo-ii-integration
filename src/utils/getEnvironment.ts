f07ec0d792712273de624392c9f2e87bcfb7e818
Yasuo Higa <higayasuo@gmail.com>
2025-03-29 17:11:27 +0900

    Refactor getEnvironment utility to remove unnecessary console logs for 'bare' execution environment. This change enhances code cleanliness and focuses on relevant checks for frontendCanisterId.

--- src/utils/getEnvironment.ts
+++ src/utils/getEnvironment.ts
@@ -5,8 +5,6 @@ export const getEnvironment = (
   console.log('executionEnvironment', executionEnvironment);
 
   if (executionEnvironment === 'bare') {
-    console.log('window.location.href', window.location.href);
-    console.log('frontendCanisterId', frontendCanisterId);
     if (window.location.href.includes(frontendCanisterId)) {
       return 'icp';
     }
